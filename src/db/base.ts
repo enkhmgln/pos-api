cd546b5ba08c496ecc34fad2b28786470cf0cd2b
enkhmgln <enkhamgalan560@gmail.com>
2026-03-08 01:06:45 +0800

    Update default value for updated_at column to current date in baseColumns

--- src/db/base.ts
+++ src/db/base.ts
@@ -3,5 +3,5 @@ import { timestamp, uuid } from "drizzle-orm/pg-core";
 export const baseColumns = {
   id: uuid().defaultRandom().primaryKey(),
   created_at: timestamp().defaultNow().notNull(),
-  updated_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull().default(new Date()),
 };
