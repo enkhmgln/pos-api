c85f5a4be3764e5229b0599abf2366d282aefa0a
enkhmgln <enkhamgalan560@gmail.com>
2026-03-08 02:27:35 +0800

    Export requireAuth middleware for enhanced authentication handling

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,4 @@
+export { requireAuth } from "./auth.middleware";
 export { globalErrorHandler } from "./error-handler.middleware";
 export { responseMiddleware } from "./response.middleware";
 export { notFoundMiddleware } from "./not-found";
